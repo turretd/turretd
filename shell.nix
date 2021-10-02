a65d5ada88b285a875fbd3091a217e2e385941be
turretd <turretdefense018@gmail.com>
2021-10-02 05:39:58 +0000

    add mic92/nixos-shell

--- shell.nix
+++ shell.nix
@@ -1,10 +1,11 @@
 let 
     sources = import ./nix/sources.nix;
     pkgs = import sources.nixpkgs {};
+    nixos-shell = import sources.nixos-shell {};
 in
 pkgs.mkShell {
     buildInputs = with pkgs; [ 
-        nixfmt niv
+        nixfmt niv nixos-shell
         terraform terragrunt awscli2 
         fd ripgrep rage sops entr gnumake
         nodejs yarn
